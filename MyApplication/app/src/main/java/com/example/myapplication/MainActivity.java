dc23005f53257753127f30ba962245f972a12c29
ZeyuCui <64547984+ZeyuCui@users.noreply.github.com>
2024-01-02 05:57:50 -0800

    Switch from activities

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,7 +40,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        
+        nextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), ScrollActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
--- MyApplication/app/src/main/java/com/example/myapplication/ScrollActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ScrollActivity.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScrollActivity extends AppCompatActivity {
 
+    Button backButtun;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scroll_activity);
+        backButtun=findViewById(R.id.backButton);
+
+        backButtun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+            }
+        });
     }
 }
